f75fcd3bc6a5785d021204f8970a5ad548a85bda
Coby Vardy <coby@dowbuilt.com>
2023-03-09 15:30:37 -0800

    initial csv output

--- product_scraper/product_scraper/spiders/rj.py
+++ product_scraper/product_scraper/spiders/rj.py
@@ -13,7 +13,16 @@ class ImdbCrawler(CrawlSpider):
         if body_class == "product":
             sku = response.xpath('//*[@id="content"]').re(r'Catalog Number:</span>(.*)</p')[0]
             title = response.xpath('/html/body/section/div[1]/div[1]/h1').re(r'>(.*)<')[0]
-            yield {"url": response.url, "body_class": body_class, "title": title, "sku":sku}
+            options = response.xpath('//*[@id="product_detail_form"]/div[3]/fieldset/div/select').re('<option value=.*>(.*)</option>')
+            if len(options) > 0:
+                for option in options:
+                    yield {"title": title, "sku":sku, "item":option}
+            else:
+                yield {"title": title, "sku":sku, "item":title}
         elif body_class is not None:
-            yield {"MESSAGE":"NOT PRODUCT","url": response.url, "body_class": body_class}
+            pass
+            # yield {"MESSAGE":"NOT PRODUCT","url": response.url, "body_class": body_class}
+
+
+# {"url": response.url, "body_class": body_class, "title": title, "sku":sku}
 
