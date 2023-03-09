edd5f5d4dde2f32fcfcf3ba7d2918d48243e9074
ArJonVar <Ariel_Vardy@yahoo.com>
2023-03-08 20:28:33 -0800

    Ariel's first

--- product_scraper/product_scraper/spiders/rj.py
+++ product_scraper/product_scraper/spiders/rj.py
@@ -1,9 +1,32 @@
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+import scrapy
+
+
+class ProductItem(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
 
 class ImdbCrawler(CrawlSpider):
     name = 'rj'
     allowed_domains = ['www.renegadejuggling.com']
     start_urls = ['https://www.renegadejuggling.com/']
-    rules = (Rule(LinkExtractor()),)
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//*[@class="product"]')),
+    )
+    # Rule(LinkExtractor(restrict_xpaths='//*[@class="product"]')),
+    # Rule(LinkExtractor()),
+    
+    def parse(self, response):
+        # Extract the product name and price from the page
+        name = response.xpath('//h1[@class="heading"]/text()').get()
+        price = response.xpath('//span[@id="subtotalPrice"]/text()').get()
+    
+        # Create an item object to store the extracted data
+        item = ProductItem()
+        item['name'] = name
+        item['price'] = price
+    
+        # Return the item to the Scrapy engine
+        yield item
